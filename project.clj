65734314f96e1ecdf26a290fa8ba120da05ffde9
Tim Berglund <tlberglund@github.com>
2013-09-22 06:13:28 -0600

    Changed license to MIT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject code "0.1.0-SNAPSHOT"
   :description "Sample code for Discrete Math You Need to Know"
   :url "http://example.com/FIXME"
-  :license {:name "CC BY-SA 3.0"
-            :url "http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"}
+  :license {:name "MIT License"
+            :url "http://choosealicense.com/licenses/mit/"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/math.combinatorics "0.0.4"]]
   :main discrete.core)
